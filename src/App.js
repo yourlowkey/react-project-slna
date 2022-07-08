e86bcb4350b4add81913c69176b8f66be7746f9b
huyd84 <huyduong0849@gmail.com>
2022-07-08 09:39:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import './node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
